b41c1fc527b3bcc2475f8edd7984591c41ce6fa8
FannyLu-na4 <f.maz@coadvil.edu.co>
2025-03-11 11:40:09 -0500

    FANNY MAZ Y SAHILY LUNA

---
+++ THONNY/CUADRADO CON CRUZ.py
@@ -0,0 +1,21 @@
+import turtle
+turtle.speed(9)
+
+def dibujar_cuadro():
+    for _ in range(1):
+        turtle.pencolor("blue")
+        turtle.goto(50, 0)
+        turtle.forward(100)
+        turtle.right(90)
+        
+        
+dibujar_cuadro()
+     
+
+    
+    
+    
+       
+     
+        
+        
---
+++ THONNY/PLANETA DOS LUNAS.py
@@ -0,0 +1,18 @@
+import turtle
+
+r= 100
+turtle.circle(r)
+
+turtle.penup()
+turtle.goto( 150, 0)
+turtle.pendown()
+r=10
+turtle.circle(r)
+
+turtle.penup()
+turtle.goto( -150, 0)
+turtle.pendown()
+r=7
+turtle.circle(r)
+
+
